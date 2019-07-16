ee92983691dbf4024770f3d6b4e946270d56827e
Jaya Swamy <JSwamy@bpl.netlink.com>
2019-07-16 12:49:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'); //sharing files between different files
+                                  // import express from 'express'; ES2015 modules
+const app = express(); // represents a running express app
+
+//route handler
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+});
+
+app.get('/greet', (req, res) => {
+  res.send({hi: 'there'});
+});
+
+/*Hereko runs our application, which has the capacity
+to inject environment variables, environment variables,
+are variables that are set underdlying
+Hereko oppourtunity to inspect and inject it
+if we are running on development environment
+we need to handle the boolean statement
+by default it will use 5000 and if env variable in
+production will run on the configured port
+*/
+const PORT  = process.env.PORT || 5000;
+app.listen(5000);
